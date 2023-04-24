2f0d83fc4893c8df93bf34ed24421916223d380e
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-24 01:21:47 -0500

    d

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -9,18 +10,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+
+
 CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["POST", "GET"])
 def messages():
-    return ''
+    if request.method == 'POST':
+            m = Message(body=request.get_json()['body'], username=request.get_json()['username'])
+            db.session.add(m)
+            db.session.commit()
+            return make_response(jsonify(m.to_dict()), 202)
+    else:
+        jso = []
+        all = Message.query.order_by(Message.created_at).all()
+        for el in all:
+            jso.append(el.to_dict())
+        return make_response(jsonify(jso), 202)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
-
+    if request.method == 'DELETE':
+        m = Message.query.filter(Message.id == id).first()
+        db.session.delete(m)
+        db.session.commit()
+        return make_response('OK', 200)
+    else:
+        m = Message.query.filter(Message.id == id).first()
+        m.body = request.get_json()['body']
+        db.session.commit()
+        return make_response(jsonify(m.to_dict()), 200)
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,5 +10,7 @@ db = SQLAlchemy(metadata=metadata)
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
-
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
